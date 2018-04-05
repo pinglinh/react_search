55b543d91233f9373c1511a4e896083fa00a3611
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-04-05 23:07:14 +0100

    Change SearchContainer to export default

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Search from "../../components/Search";
 
-export class SearchContainer extends React.Component {
+class SearchContainer extends React.Component {
   state = {
     articles: []
-  }
+  };
 
   performSearch = query => {
     fetch(`http://localhost:3456/?searchTerm=${query}`)
@@ -14,41 +14,9 @@ export class SearchContainer extends React.Component {
       });
   };
 
-  // handleChange(event) {
-  //   console.log(event);
-  //   this.setState({ value: event.target.value });
-  //   fetch(`http://localhost:3456/?searchTerm=${event.target.value}`)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       this.setState({ articles: data.response.results });
-  //     });
-  // }
-
-  // handleChange(event) {
-  //   console.log(event);
-  //   this.setState({ value: event.target.value });
-  //   fetch(
-  //     `http://content.guardianapis.com/search?q=${
-  //       event.target.value
-  //     }&api-key=3e6177a5-bcb1-493d-ad07-20c4fab62575`
-  //   )
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log(data.response.results);
-  //       return data;
-  //     })
-  //     .then(data => this.setState({ articles: data.response.results }));
-  // }
-
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  // }
-
   render() {
     return (
       <Search
-        // handleSubmit={this.handleSubmit}
-        // value={this.state.value}
         performSearch={this.performSearch}
         articles={this.state.articles}
       />
@@ -56,4 +24,4 @@ export class SearchContainer extends React.Component {
   }
 }
 
-// responsible for fetching the data and passing the data to the child which is the search component
+export default SearchContainer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { SearchContainer } from "./containers/Search";
+import SearchContainer from "./containers/Search";
 
 import style from "./App.css";
 
